f585f5839a5e2342529e489047b47bc83c8f6e90
Scott Duffey <scottrduffey@gmail.com>
2023-03-15 08:33:54 +0900

    Enable interrupts

---
+++ gdt.asm
@@ -0,0 +1,11 @@
+gdtr DW 0 ; For limit storage
+     DD 0 ; For base storage
+
+global setGdt
+setGdt:
+   MOV   AX, [esp + 4]
+   MOV   [gdtr], AX
+   MOV   EAX, [ESP + 8]
+   MOV   [gdtr + 2], EAX
+   LGDT  [gdtr]
+   RET
--- kernel.c
+++ kernel.c
@@ -169,51 +169,139 @@ void read_mac_address(volatile uint32_t * eeprom_address, uint8_t * mac_address)
 	mac_address[5] = value >> 8;
 }
 
+struct idt_entry_t {
+	uint16_t isr_low;	// The lower 16 bits of the ISR's address
+	uint16_t kernel_cs;	// The GDT segment selector that the CPU will load into CS before calling the ISR
+	uint8_t reserved;	// Set to zero
+	uint8_t attributes;	// Type and attributes; see the IDT page
+	uint16_t isr_high;	// The higher 16 bits of the ISR's address
+} __attribute__((packed));
+
+struct idtr_t {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+
+__attribute__((aligned(0x10)))
+static struct idt_entry_t idt[256];	// Create an array of IDT entries; aligned for performance
+
+static struct idtr_t idtr;
+
+void exception_handler(void);
+void exception_handler()
+{
+	terminal_writestring("hello idt");
+	__asm__ volatile ("cli; hlt");	// Completely hangs the computer
+}
+
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags);
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
+{
+	struct idt_entry_t *descriptor = &idt[vector];
+
+	descriptor->isr_low = (uint32_t) isr & 0xFFFF;
+	descriptor->kernel_cs = 0x08;	// this value can be whatever offset your kernel code selector is in your GDT
+	descriptor->attributes = flags;
+	descriptor->isr_high = (uint32_t) isr >> 16;
+	descriptor->reserved = 0;
+}
+
+extern void *isr_stub_table[];
+
+void idt_init(void);
+void idt_init()
+{
+	idtr.base = (uintptr_t) & idt[0];
+	idtr.limit = (uint16_t) sizeof(idt) - 1;
+
+	for (uint8_t vector = 0; vector < 32; vector++) {
+		idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
+		//vectors[vector] = true;
+	}
+
+	__asm__ volatile ("lidt %0"::"m" (idtr));	// load the new IDT
+	__asm__ volatile ("sti");	// set the interrupt flag
+}
+
+void encode_gdt_entry(uint8_t * target, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
+{
+	// Encode the limit
+	target[0] = limit & 0xFF;
+	target[1] = (limit >> 8) & 0xFF;
+	target[6] = (limit >> 16) & 0x0F;
+
+	// Encode the base
+	target[2] = base & 0xFF;
+	target[3] = (base >> 8) & 0xFF;
+	target[4] = (base >> 16) & 0xFF;
+	target[7] = (base >> 24) & 0xFF;
+
+	// Encode the access byte
+	target[5] = access;
+
+	// Encode the flags
+	target[6] |= (flags << 4);
+}
+
+void setGdt(uint32_t, uint32_t);
+
 void kernel_main(void)
 {
+	uint8_t gdt[8 * 3];
+	encode_gdt_entry(gdt, 0, 0, 0, 0);
+	encode_gdt_entry(gdt + 8, 0, 0xFFFFF, 0x9A, 0xC);
+	encode_gdt_entry(gdt + 16, 0, 0xFFFFF, 0x92, 0xC);
+
+	setGdt(sizeof(gdt) - 1, (uint32_t) gdt);
+	idt_init();
+
 	/* Initialize terminal interface */
 	terminal_initialize();
 
-	uint16_t bus;
-	uint8_t device;
-	uint8_t function = 0;
+	__asm__("int $0");
 
-	for (bus = 0; bus < 256; bus++) {
-		for (device = 0; device < 32; device++) {
-			uint16_t vendor_id = get_vendor_id(bus, device, function);
+	/*
+	   uint16_t bus;
+	   uint8_t device;
+	   uint8_t function = 0;
 
-			if (vendor_id == 0xFFFF)
-				continue;
+	   for (bus = 0; bus < 256; bus++) {
+	   for (device = 0; device < 32; device++) {
+	   uint16_t vendor_id = get_vendor_id(bus, device, function);
 
-			uint16_t device_id = get_device_id(bus, device, function);
-			uint16_t command = get_command(bus, device, function);
-			uint8_t header_type = get_header_type(bus, device, function);
+	   if (vendor_id == 0xFFFF)
+	   continue;
 
-			terminal_printf("bus: %d device %d %04X %04X %04X %02X", bus, device, vendor_id, device_id, command, header_type);
-			terminal_writestring("\n");
+	   uint16_t device_id = get_device_id(bus, device, function);
+	   uint16_t command = get_command(bus, device, function);
+	   uint8_t header_type = get_header_type(bus, device, function);
 
-			if (header_type != 0x00)
-				continue;
+	   terminal_printf("bus: %d device %d %04X %04X %04X %02X", bus, device, vendor_id, device_id, command, header_type);
+	   terminal_writestring("\n");
 
-			for (int i = 0; i < 6; i++) {
-				uint32_t bar = get_bar_n(bus, device, function, i);
+	   if (header_type != 0x00)
+	   continue;
 
-				terminal_printf("    %08X\n", bar);
-			}
-		}
-	}
+	   for (int i = 0; i < 6; i++) {
+	   uint32_t bar = get_bar_n(bus, device, function, i);
 
-	uint32_t memory_base_address = get_bar_n(0, 3, 0, 0) & ~3;
-	volatile uint32_t *eeprom_address = (uint32_t *) (memory_base_address + 0x0014);
+	   terminal_printf("    %08X\n", bar);
+	   }
+	   }
+	   }
 
-	terminal_printf("Memory Base Address: %08X\n", memory_base_address);
-	terminal_printf("EEPROM: %p\n", eeprom_address);
+	   uint32_t memory_base_address = get_bar_n(0, 3, 0, 0) & ~3;
+	   volatile uint32_t *eeprom_address = (uint32_t *) (memory_base_address + 0x0014);
 
-	uint8_t mac_address[6];
-	read_mac_address(eeprom_address, mac_address);
-	terminal_printf("MAC address %02X:%02X:%02X:%02X:%02X:%02X\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
+	   terminal_printf("Memory Base Address: %08X\n", memory_base_address);
+	   terminal_printf("EEPROM: %p\n", eeprom_address);
 
-	volatile uint32_t *mta_address = (uint32_t *) (memory_base_address + 0x5200);
-	for (int i = 0; i < 0x80; i++)
-		*mta_address = 0;
+	   uint8_t mac_address[6];
+	   read_mac_address(eeprom_address, mac_address);
+	   terminal_printf("MAC address %02X:%02X:%02X:%02X:%02X:%02X\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
+
+	   volatile uint32_t *mta_address = (uint32_t *) (memory_base_address + 0x5200);
+	   for (int i = 0; i < 0x80; i++)
+	   *mta_address = 0;
+	 */
 }
