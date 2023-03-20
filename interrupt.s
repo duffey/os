fc64ae36449d4f5921c7969de20c02009e1392bc
Scott Duffey <scottrduffey@gmail.com>
2023-03-21 08:34:00 +0900

    Don't call sti or cli in interrupt handler

--- interrupt.s
+++ interrupt.s
@@ -8,7 +8,6 @@
 %macro ISR_NOERRCODE 1
   global isr%1
   isr%1:
-    cli                         ; Disable interrupts firstly.
     push byte 0                 ; Push a dummy error code.
     push byte %1                ; Push the interrupt number.
     jmp isr_common_stub         ; Go to our common handler code.
@@ -19,7 +18,6 @@
 %macro ISR_ERRCODE 1
   global isr%1
   isr%1:
-    cli                         ; Disable interrupts.
     push byte %1                ; Push the interrupt number
     jmp isr_common_stub
 %endmacro
@@ -29,7 +27,6 @@
 %macro IRQ 2
   global irq%1
   irq%1:
-    cli
     push byte 0
     push byte %2
     jmp irq_common_stub
@@ -114,7 +111,6 @@ isr_common_stub:
 
     popa                     ; Pops edi,esi,ebp...
     add esp, 8     ; Cleans up the pushed error code and pushed ISR number
-    sti
     iret           ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
 
 ; In isr.c
@@ -147,7 +143,6 @@ irq_common_stub:
 
     popa                     ; Pops edi,esi,ebp...
     add esp, 8     ; Cleans up the pushed error code and pushed ISR number
-    sti
     iret           ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
 
 
